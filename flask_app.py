fad991381ff1eb30cc8b42bf0a31919ed786fff5
kalumba Samuel <kalumbasml@gmail.com>
2024-03-14 16:21:31 +0300

    basic methods

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,5 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from markupsafe import escape
+from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
@@ -9,3 +7,26 @@ app = Flask(__name__)
 def hello_world():
     return '<h1>Hello from Flask!</h1>'
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
+
+#print(url_for('show_user_profile', username='Sam'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
