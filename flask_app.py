4c034db634ac6265c6fc091b910e9611595ea439
kalumba Samuel <kalumbasml@gmail.com>
2024-03-14 16:44:20 +0300

    basic methods

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,6 @@
 from markupsafe import escape
-from flask import Flask, url_for, request
+from werkzeug.utils import secure_filenam
+from flask import Flask, url_for, request, render_template
 
 app = Flask(__name__)
 
@@ -23,10 +24,91 @@ def show_subpath(subpath):
     return f'Subpath {escape(subpath)}'
 
 #print(url_for('show_user_profile', username='Sam'))
+#url_for('static', filename='style.css')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html, name=name)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['the_file']
+        #f.save('/var/www/uploads/uploaded_file.txt')
+        f.save(f"/var/www/uploads/{secure_filename(f.filename)}")
+
+#from flask import abort, redirect, url_for
+
+#@app.route('/')
+#def index():
+#    return redirect(url_for('login'))
+
+#@app.route('/login')
+#def login():
+ #   abort(401)
+ #    this_is_never_executed()
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
+@app.route("/me")
+def me_api():
+    user = get_current_user()
+    return {
+        "username": user.username,
+        "theme": user.theme,
+        "image": url_for("user_image", filename=user.image),
+    }
+
+@app.route("/users")
+def users_api():
+    users = get_all_users()
+    return [user.to_json() for user in users]
+
+from flask import session
+
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return f'Logged in as {session["username"]}'
+    return 'You are not logged in'
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return do_the_login()
-    else:
-        return show_the_login_form()
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+'''python -c 'import secrets; print(secrets.token_hex())'
+'192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf'
+'''
