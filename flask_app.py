d801bc0a4653322dd7a487a06e83b75da35dbcc6
kalumba Samuel <kalumbasml@gmail.com>
2024-03-14 17:04:38 +0300

    sqlite functionality

--- flask_app.py
+++ flask_app.py
@@ -112,3 +112,49 @@ def logout():
 '''python -c 'import secrets; print(secrets.token_hex())'
 '192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf'
 '''
+
+#DB
+import sqlite3
+from flask import g
+
+DATABASE = '/path/to/database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@app.route('/')
+def index():
+    cur = get_db().cursor()
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+'''
+for user in query_db('select * from users'):
+    print(user['username'], 'has the id', user['user_id'])
+
+user = query_db('select * from users where username = ?',
+                [the_username], one=True)
+if user is None:
+    print('No such user')
+else:
+    print(the_username, 'has the id', user['user_id'])
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+¹'''
